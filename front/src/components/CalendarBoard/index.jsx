ad62c2f45c12992e1be68042fc082b958955a319
Tsukasa311 <70062121+Tsukasa311@users.noreply.github.com>
2021-03-04 17:56:00 +0900

    ロジック切り離し

--- front/src/components/CalendarBoard/index.jsx
+++ front/src/components/CalendarBoard/index.jsx
@@ -1,25 +1,10 @@
 import React from 'react';
 import { GridList, StylesProvider} from '@material-ui/core';
-import * as styles from "./style.css";
-import dayjs from 'dayjs';
 import CalendarElement from '../CalendarElement';
-import "dayjs/locale/ja";
+import { createCalendar } from "../../services/calendar";
+import * as styles from "./style.css";
 
-dayjs.locale("ja");
 
-const createCalendar = () => {
-  const firstDay = dayjs().startOf("month");
-  const firstDayIndex = firstDay.day();
-
-  return Array(35)
-  .fill(0)
-  .map((_, i) => {
-    const diffFromFirstDay = i - firstDayIndex;
-    const day = firstDay.add(diffFromFirstDay, "day");
-
-    return day;
-  });
-};
 const calendar = createCalendar();
 
 const CalendarBoard = () => {
--- front/src/components/CalendarElement/index.jsx
+++ front/src/components/CalendarElement/index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import * as styles from "./style.css";
 
 const CalendarElement = ({ children }) => {
