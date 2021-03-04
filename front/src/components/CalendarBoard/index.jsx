d2b362b60eaad1019c2788c80dd96144960ea155
Tsukasa311 <70062121+Tsukasa311@users.noreply.github.com>
2021-03-04 18:51:45 +0900

    表示を整える

--- front/src/components/CalendarBoard/index.jsx
+++ front/src/components/CalendarBoard/index.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
-import { GridList, StylesProvider} from '@material-ui/core';
+import { GridList, StylesProvider, Typography } from '@material-ui/core';
 import CalendarElement from '../CalendarElement';
 import { createCalendar } from "../../services/calendar";
 import * as styles from "./style.css";
 
 
 const calendar = createCalendar();
+const days = ["日", "月", "火", "水", "木", "金", "土"];
 
 const CalendarBoard = () => {
   return(
     <div className={styles.container}>
       <GridList className={styles.grid} cols={7} spacing={0} cellHeight="auto">
-        {calendar.map(c => (
-          <li key={c.toISOString()}>
-            <CalendarElement>{c.format("D")}</CalendarElement>
+      {days.map(d => (
+          <li key={d}>
+            <Typography
+              className={styles.days}
+              color="textSecondary"
+              align="center"
+              variant="caption"
+              component="div"
+            >
+              {d}
+            </Typography>
           </li>
+        ))}
+        {calendar.map(c => (
+             <li key={c.toISOString()}>
+             <CalendarElement day={c} />
+           </li>
         ))}
       </GridList>
     </div>
